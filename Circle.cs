0bcf6055274dc848a572952887346cc3bddfc44e
Gregory Wizner <wiznergregory@gmail.com>
2023-02-09 19:34:26 -0500

    Circle lab

---
+++ Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleLab2023
+{
+    public class Circle
+    {
+        public double rad;
+        public double circ;
+        public double area;
+
+        public Circle(double radius)
+        {
+            rad = radius;
+        }
+        public double CalculateCircumference()
+        {
+            circ = 2 * Math.PI * rad;
+            return circ;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            string roundCirc = FormatNumber(CalculateCircumference());
+            return "The " + "\x1b[38;5;207m" + "circumference " + "\x1b[0m" + "is " +
+                "\x1b[38;5;207m" + $"{roundCirc}" + "\x1b[0m";
+        }
+        public double CalculateArea()
+        {
+            area = Math.PI * Math.Pow(rad, 2);
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            string roundArea = FormatNumber(CalculateArea());
+            return "The " + "\x1b[38;5;129m" + "area " + "\x1b[0m" + "is " + "\x1b[38;5;129m" +
+                $"{roundArea}" + "\x1b[0m";
+        }
+        private string FormatNumber(double x)
+        {
+            string round = Math.Round(x, 2).ToString();
+            return round;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Reflection;
+
+namespace CircleLab2023
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Circle> totCircles = new List<Circle>();
+            bool keepAsk = true;
+
+            Console.WriteLine("Welcome to \"Circle Builder.\"\n");
+            while (keepAsk)
+            {
+                double userRad = Validator.getUserRad();
+
+                Circle newCircle = new Circle(userRad);
+
+                string trueCirc = newCircle.CalculateFormattedCircumference();
+                string trueArea = newCircle.CalculateFormattedArea();
+
+                Console.WriteLine();
+                Console.WriteLine(trueCirc);
+                Console.WriteLine();
+                Console.WriteLine(trueArea);
+
+                totCircles.Add(newCircle);
+
+                keepAsk = Validator.getContinue();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"You have built " + "\x1b[38;5;196m" + $"{totCircles.Count} " +
+                "\x1b[38;5;200m" + "circle(s). " + "\x1b[0m" + "Goodbye.");
+
+        }
+    }
+}
